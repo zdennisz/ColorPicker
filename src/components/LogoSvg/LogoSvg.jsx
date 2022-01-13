dfa64fe544010adb6fd3351cdc98605bb6d51e0e
Dennis Z <zdennisz@gmail.com>
2022-01-13 22:48:40 +0200

    resized the background & removed console print

--- src/components/LogoSvg/LogoSvg.jsx
+++ src/components/LogoSvg/LogoSvg.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ReactComponent as Chamelleon } from "./../../assets/chamelleon.svg";
 
 const LogoSvg = ({ width, height, fill }) => {
-	console.log("fill", fill);
 	return <Chamelleon width={width} height={height} fill={fill} />;
 };
 
